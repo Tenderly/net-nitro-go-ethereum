8615472eec66f4b4793ceacb2c3ae3ec1b5c95cd
pdrobnjak <drobnjakpavle95@gmail.com>
2024-09-04 19:34:49 +0200

    Remove unused import

--- eth/gasestimator/gasestimator.go
+++ eth/gasestimator/gasestimator.go
@@ -25,7 +25,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/log"
